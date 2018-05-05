1f647be53a5956f434c3ab34090a1129fdbaaf54
Henning Leutz <leutz@pcsg.de>
2018-05-05 16:00:08 +0200

    feat: new option block start at bot init starting, this makes it possible to start the blockchain parsing at a specific block number

--- run.php
+++ run.php
@@ -2,9 +2,33 @@
 
 require_once dirname(__FILE__)."/vendor/autoload.php";
 
-$Parser = new \PCSG\SteemBlockchainParser\Parser();
+try {
+    $Parser = new \PCSG\SteemBlockchainParser\Parser();
+    $Config = PCSG\SteemBlockchainParser\Config::getInstance();
+} catch (\Exception $Exception) {
+    echo $Exception->getMessage();
+    echo PHP_EOL;
+    exit;
+}
+
+// db test
+try {
+    $Parser->getDatabase();
+} catch (\Exception $Exception) {
+    echo $Exception->getMessage();
+    echo PHP_EOL;
+    exit;
+}
+
 
 $lastBlock = $Parser->getLatestBlockFromDatabase();
+
+if ($lastBlock === 0) {
+    try {
+        $lastBlock = $Config->get('block', 'start');
+    } catch (\Exception $Exception) {
+    }
+}
 
 $Parser->parseBlockRangeAsync(
     $lastBlock + 1,
